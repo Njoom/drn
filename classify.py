515559abf77d614885a9186138ba9fbb97848b71
Njoom <85446073+Njoom@users.noreply.github.com>
2026-01-28 20:27:39 +0300

    Update classify.py

--- classify.py
+++ classify.py
@@ -306,7 +306,7 @@ def validate(args, test_loader, model, criterion):
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
-                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   i, len(test_loader), batch_time=batch_time, loss=losses,
                    top1=top1))
 
     print(' * Prec@1 {top1.avg:.3f}'.format(top1=top1))
